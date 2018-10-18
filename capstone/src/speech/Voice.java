6c95355386563d7bf61df2752461ac8e1345e7e6
tyre king <tyr3.k1ng@gmail.com>
2018-10-18 12:37:11 -0400

    Created file to start voice recognition.

---
+++ capstone/src/speech/Voice.java
@@ -0,0 +1,13 @@
+package speech;
+
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+
+public class Voice {
+    
+    private LiveSpeechRecognizer recognizer;
+
+    public Voice() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
