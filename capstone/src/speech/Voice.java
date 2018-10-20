5ee6c729d74df589c125f6791f121c216e3c933d
tyre king <tyr3.k1ng@gmail.com>
2018-10-20 13:03:34 -0400

    set up configurations for recognizer object

--- capstone/src/speech/Voice.java
+++ capstone/src/speech/Voice.java
@@ -1,13 +1,45 @@
 package speech;
 
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 
 public class Voice {
-    
+    private String speechResult;
     private LiveSpeechRecognizer recognizer;
+    private Logger logger =Logger.getLogger(getClass().getName()); 
+    
+    private ExecutorService eventsExecutorService = Executors.newFixedThreadPool(2);
 
     public Voice() {
-        // TODO Auto-generated constructor stub
+        Configuration configs = new Configuration();
+        configs.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configs.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        
+        
+        configs.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+        
+        try {
+            recognizer = new LiveSpeechRecognizer(configs);
+                    
+        }catch (IOException e) {
+           logger.log(Level.SEVERE, "Reconizer with mic did not set Properly", e);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 }
