a0f36e68f0335ec00075343aa9dc200cfd45b7e0
tyre king <tyr3.k1ng@gmail.com>
2018-10-21 13:29:41 -0400

    added icons

--- capstone/src/application/HomeController.java
+++ capstone/src/application/HomeController.java
@@ -1,13 +1,23 @@
 package application;
 
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.glass.ui.TouchInputSupport;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.DataLine.Info;
+import javax.sound.sampled.TargetDataLine;
+
+
 
 import javafx.fxml.FXML;
 
 import javafx.scene.image.ImageView;
+import twitter4j.TwitterException;
+import twitteraApp.Permissions;
 
 public class HomeController {
 
@@ -16,6 +26,26 @@ public class HomeController {
 
     @FXML
     private ImageView Logo;
+    @FXML
+    private ImageView googleCalendarIcon;
+
+    @FXML
+    private ImageView weatherIcon;
+
+    @FXML
+    private ImageView timeDateIcon;
+
+    @FXML
+    private ImageView facebookicon;
+
+    @FXML
+    private ImageView twitterIcon;
+
+    @FXML
+    private ImageView youtubeIcon;
+
+    @FXML
+    private ImageView settingsIcon;
     
     List<ImageView> pics = new ArrayList<>();
    
@@ -25,23 +55,42 @@ public class HomeController {
     }
     
     
-    public void highlighted(String move) {
-        
+    public void highlighted(String move) throws MalformedURLException, TwitterException {
+        Permissions permissions = new Permissions();
             pics.add(exit);
-            pics.add(Logo);
+            pics.add(weatherIcon);
+            pics.add(timeDateIcon);
+            pics.add(facebookicon);
+            pics.add(twitterIcon);
+            pics.add(youtubeIcon);
+            pics.add(googleCalendarIcon);
+            pics.add(settingsIcon);
+            
+            
+            
+            
+           
+            
         
             
         
         if(move.equals("left")) {
             exit.setOpacity(1);
             Logo.setOpacity(.5);
+            
         }
         if (move.equals("right")) {
             exit.setOpacity(.5);
             Logo.setOpacity(1);
+            
+        }
+        if(move.equals("up")) {
+            
         }
         
         
     }
+    
+    
    
 }
