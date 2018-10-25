9d457d1676b6f318c2d910d8b1eb0bec744ea82c
tyre king <tyr3.k1ng@gmail.com>
2018-10-25 11:41:07 -0400

    changed to be called and used from the Homecontroller controller

--- capstone/src/twitteraApp/Permissions.java
+++ capstone/src/twitteraApp/Permissions.java
@@ -2,6 +2,11 @@ package twitteraApp;
 
 
 
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -9,17 +14,24 @@ import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 
+
 public class Permissions {
 
     
-    public static void main(String[]args) throws TwitterException {
-        Permissions permissions = new Permissions();
-    }
-    public Permissions() throws TwitterException {
-       String tweeted = createTweet("tweeting from my application");
-       System.out.println(tweeted);
-    }
+//    public static void main(String[]args) throws TwitterException {
+//        Permissions permissions = new Permissions("test3");
+//    }
+//    public Permissions() throws TwitterException {
+//       String tweeted = createTweet("test 2");
+//       System.out.println(tweeted);
+//    }
     
+    public Permissions(String tw) throws TwitterException{
+        String tweeted = createTweet(tw);
+        System.out.println(tweeted);
+        
+         
+    }
 
     public String createTweet(String tweet) throws TwitterException {
         Twitter twitter = getTwitterinstance();
@@ -27,22 +39,36 @@ public class Permissions {
         return status.getText();
     }
 
-    private Twitter getTwitterinstance() {
+ 
+   
+    public List<String> getTimeLine() throws TwitterException {
+        Twitter twitter = getTwitterinstance();
+         
+        return twitter.getHomeTimeline().stream()
+          .map(item -> item.getText())
+          .collect(Collectors.toList());
+    }
+
+//    public Image getUserPic() throws TwitterException, MalformedURLException{
+//        Twitter twitter = getTwitterinstance();
+//        User user = twitter.showUser(twitter.getId());
+//        Image image = new Image(user.getProfileImageURL());
+//        return image;
+//    }
+//   
+       
+           private Twitter getTwitterinstance() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true);
-        cb.setOAuthConsumerKey(System.getenv("oauth.consumerKey"));
-        cb.setOAuthConsumerSecret(System.getenv("oauth.conumerSecret"));
-        cb.setOAuthAccessToken(System.getenv("oauth.accessToken"));
-        cb.setOAuthAccessTokenSecret(System.getenv("oauth.accessTokenSecret"));
+//        cb.setDebugEnabled(true);
+//        cb.setOAuthConsumerKey(System.getenv("CK"));
+//        cb.setOAuthConsumerSecret(System.getenv("CKS"));
+//        cb.setOAuthAccessToken(System.getenv("AT"));
+//        cb.setOAuthAccessTokenSecret(System.getenv("ATS"));
         
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
         return twitter;
     }
-   
-
-       
-        
         
 }
 
