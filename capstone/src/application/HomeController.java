cdff6d2379415089aa9832792e9c37b490e1661b
tyre king <tyr3.k1ng@gmail.com>
2018-10-28 13:33:53 -0400

    added animations to LEFT Right movements

--- capstone/src/application/HomeController.java
+++ capstone/src/application/HomeController.java
@@ -9,9 +9,14 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.DataLine.Info;
+
+import edu.cmu.sphinx.linguist.acoustic.tiedstate.HTK.Lab;
+
 import javax.sound.sampled.TargetDataLine;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -53,13 +58,14 @@ public class HomeController {
     private Label tweetLabel;
     @FXML
     private TextField tweetTextField;
-
-
+    @FXML
+    public Label showCommand;
     
     List<ImageView> pics = new ArrayList<>();
    
     private int position=0;
     private boolean twitterActivated=false;
+
     
 
     public  HomeController getController() {
@@ -71,22 +77,44 @@ public class HomeController {
         pics.add(youtubeIcon);
         pics.add(googleCalendarIcon);
         pics.add(settingsIcon);
+        
         return this;
     }
     
-    
+
+    //All animations are to be placed in its own class
+    public void fadein(Label label) {
+        FadeTransition ft = new FadeTransition(Duration.millis(3000),label);
+        ft.setFromValue(0.1);
+        ft.setToValue(1.0);
+       // ft.setCycleCount(Timeline.INDEFINITE);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
+    public void fadeout(Label label) {
+        FadeTransition ft = new FadeTransition(Duration.millis(3000),label);
+        ft.setFromValue(0.1);
+        ft.setToValue(1.0);
+       // ft.setCycleCount(Timeline.INDEFINITE);
+        ft.setAutoReverse(false);
+        ft.play();
+    }
+    //End of animations
     
     public void highlighted(String move) throws MalformedURLException, TwitterException {
         
 
 
         if(move.equals("left")) {
-            
+            showCommand.setVisible(true);
+           Platform.runLater(()->showCommand.setText("LEFT"));
+            Platform.runLater(()->fadein(showCommand));
             prev();
             
         }
         if (move.equals("right")) {
             
+            Platform.runLater(()-> showCommand.setText("RIGHT"));
             next();
             
             
@@ -101,16 +129,21 @@ public class HomeController {
         
     }
     
+
+    
     private void complete() throws TwitterException {
         if (twitterActivated) {
             twitterActivated=false;
             tweetLabel.setVisible(false);
            tweetTextField.setOpacity(0);
             
+           
+            
             Permissions tweet = new Permissions(tweetTextField.getText());
             
             
             System.out.println("Tweet [ " +tweetTextField.getText()+" ] was sucessfully sent out.");
+           
             
         }
         
