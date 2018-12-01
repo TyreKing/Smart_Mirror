717f12c2b4d3f84260316997ca77ddf9eb841dc4
tyre king <tyr3.k1ng@gmail.com>
2018-12-01 14:05:23 -0500

    created Javadoc

--- capstone/src/weatherApp/WeatherClass.java
+++ capstone/src/weatherApp/WeatherClass.java
@@ -1,5 +1,8 @@
 package weatherApp;
 
+/**The Service being used is OpenWeatherMap
+ * OpenWeatherMap.org
+ */
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Date;
@@ -11,9 +14,20 @@ import net.aksingh.owmjapis.CurrentWeather;
 import net.aksingh.owmjapis.OpenWeatherMap;
 
 public class WeatherClass {
-
+    
+    //Global Weather Object
     private CurrentWeather currentWeather;
 
+    /**
+     * Constructor class
+     * 
+     * <p>This method creates the Weather class object
+     * 
+     * @param city  The name of the city that will be used to get the weather
+     * @param key   The OpenWeatherMap API key
+     * @throws IOException
+     * @throws JSONException
+     */
     public WeatherClass(String city, String key)
             throws IOException, JSONException {
 
@@ -22,31 +36,51 @@ public class WeatherClass {
 
     }
 
+    /**
+     * This method returns a DATE object.
+     * @return  the time and date in the set city.
+     */
     public Date getTime() {
-        // TODO Auto-generated method stub
-
         return this.currentWeather.getDateTime();
-
+        
     }
 
+    /**
+     * This method returns a Double
+     * @return  the temperature in the set city.
+     */
     public double getTemp() {
         return this.currentWeather.getMainInstance().getTemperature();
     }
-
-    
+    /**
+     * This method returns a String
+     * @return the name of the city 
+     */
     public String getCity() {
         return this.currentWeather.getCityName();
     }
-    
-    
-    public double getRain() {
-        return this.currentWeather.getRainInstance().getRain();
-    }
-    
-    
+   
+    /**
+     * returns the temperature high of the main instance of the weather object
+     * @return  the temperature high 
+     */
     public double getTempHigh() {        
         return this.currentWeather.getMainInstance().getMaxTemperature();
     }
+    /**
+     * This method returns the type of weather of the set city.
+     * @return the name of the weather type
+     */
+   
+    public String getWeatherType() {
+        return this.currentWeather.getWeatherInstance(0).getWeatherName();
+    }
+    /**
+     * The method provides a description of the current weather.
+     * @return  the description of the current weather in the set city
+     */
+    public String getDescription() {
+        return this.currentWeather.getWeatherInstance(0).getWeatherDescription();
+    }
     
-
 }
